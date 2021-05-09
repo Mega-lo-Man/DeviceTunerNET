d9afc222480771b880d8a71029b4803162e32cdc
Megalomaniac Schtkan <texview@yandex.ru>
2021-05-09 21:03:03 +0300

    Override GetHashCode for SimplestComponent

--- SharedDataModels/DeviceTunerNET.SharedDataModel/SimplestСomponent.cs
+++ SharedDataModels/DeviceTunerNET.SharedDataModel/SimplestСomponent.cs
@@ -55,16 +55,6 @@ namespace DeviceTunerNET.SharedDataModel
             set { _cabinetDesignation = value; }
         }
 
-        public override bool Equals(Object obj)
-        {
-            if (obj == null)
-                return false;
-            if (object.ReferenceEquals(this, obj))
-                return true;
-            if (this.GetType() != obj.GetType())
-                return false;
-            return this.Equals(obj as SimplestСomponent);
-        }
         public bool Equals(SimplestСomponent obj)
         {
             if (obj == null)
@@ -79,6 +69,22 @@ namespace DeviceTunerNET.SharedDataModel
                 return true;
             else
                 return false;
+        }
+
+        public override int GetHashCode()
+        {
+            int hashCode = 1485357867;
+            hashCode = hashCode * -1521134295 + _id.GetHashCode();
+            hashCode = hashCode * -1521134295 + Id.GetHashCode();
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_model);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Model);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_deviceType);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(DeviceType);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_designation);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(Designation);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(_cabinetDesignation);
+            hashCode = hashCode * -1521134295 + EqualityComparer<string>.Default.GetHashCode(ParentCabinetDesignation);
+            return hashCode;
         }
     }
 }
